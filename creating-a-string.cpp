ef8bb668d41ea4e1f8387519742c842563822454
Divyanshu raj <98219015+its-divyanshu-raj@users.noreply.github.com>
2023-01-21 21:25:15 +0530

    Create creating-a-string.cpp

---
+++ creating-a-string.cpp
@@ -0,0 +1,23 @@
+/* Strings are used for storing text.
+A string variable contains a collection of characters surrounded by double quotes.
+Before declaring a variable as string the string datatype must be used
+
+for eg - string name = "Chef";
+Write a program which does the following
+
+Create a string named 
+fruit and assign the value "Apple" to it.
+Output fruit to the console. */ 
+
+
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string fruit = "Apple";
+    cout<< fruit ;
+    return 0;
+}
