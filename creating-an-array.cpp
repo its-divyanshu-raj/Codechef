fda25cd0dc24f43770591c9ec2cf421c8ecddef9
Divyanshu raj <98219015+its-divyanshu-raj@users.noreply.github.com>
2023-01-23 16:04:59 +0530

    Create creating-an-array.cpp

---
+++ creating-an-array.cpp
@@ -0,0 +1,20 @@
+/* Arrays are used to store multiple values in a single variable, instead of declaring separate variables for each value.
+To declare an array,
+Define the variable type,
+Specify the name of the array followed by square brackets and
+Specify the number of elements it should store.
+For eg
+ string Cars[4] = {"Ferrari","Volvo","Lambo","Tesla"}
+Write a program which does the following
+Create an Array of the first 5 positive integers
+Once the array is define - output "Done" to the console. */
+
+#include<iostrean>
+#include<string>
+using namespace std;
+
+int main(){
+int num[ 5 ] = { 1, 2, 3, 4, 5 } ;
+  cout<< "done" ;
+  return 0;
+}
